4103bbc8f43b25c6f48306afdde7d42de47e12ea
Fatih AKÇE <56806691+fatihakce35@users.noreply.github.com>
2023-04-07 12:12:20 +0300

    updating code to clean code

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from discord.ext.commands import Bot, has_permissions, CheckFailure
 import bot_messages,functions
 
 from functions import *
-intents = discord.Intents.default()
-intents = discord.Intents(messages=True,guilds=True,reactions=True,members=True,presences=True)
+
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
 
  #getting discord elements
 
-Bot = commands.Bot(command_prefix = "!",intents=intents, help_command=None) #using "!" for bot command.
+Bot = commands.Bot(command_prefix = "!", intents = intents, help_command = None) #using "!" for bot command.
 
 @Bot.event
 async def on_ready():
@@ -21,7 +21,7 @@ async def on_ready():
 
 
 @Bot.command() #help command
-@has_permissions(administrator=True)
+@has_permissions(administrator = True)
 async def help(ctx):
   await ctx.channel.send(bot_messages.bot_help)
 
@@ -30,18 +30,18 @@ async def help(ctx):
 
 
 @Bot.command()
-@has_permissions(administrator=True) # you can select which user can use this command. i selected admin.
-async def command_channel(ctx,arg1: discord.TextChannel): #command channel to use bot command.
+@has_permissions(administrator = True) # you can select which user can use this command. i selected admin.
+async def command_channel(ctx, arg1: discord.TextChannel): #command channel to use bot command.
 
   file_create(Bot)
   command_channel_ids = command_channel_list(ctx.message.guild.id)
 
 
-  if len(command_channel_ids)>=1:
+  if len(command_channel_ids) >= 1:
     await ctx.channel.send(bot_messages.command_channel_set)
     return
 
-  if arg1==None:
+  if arg1 == None:
     await ctx.channel.send(bot_messages.parametres_error)
     return
 
@@ -51,19 +51,19 @@ async def command_channel(ctx,arg1: discord.TextChannel): #command channel to us
 
 
 @Bot.command()
-@has_permissions(administrator=True)
+@has_permissions(administrator = True)
 async def add_channel(ctx,arg1: discord.TextChannel, arg2: discord.TextChannel,arg3 = None,arg4=None,encoding="utf8"): #4 parametres for bot working
 
   guild_id = ctx.message.guild.id
   command_channel_ids = command_channel_list(guild_id)[0]
 
-  if len(command_channel_ids)==0:
+  if len(command_channel_ids) == 0:
     await ctx.channel.send(bot_messages.set_command)
     return
-  if ctx.channel.id !=int(command_channel_ids[0]): #checking if channel is command channel
+  if ctx.channel.id != int(command_channel_ids[0]): #checking if channel is command channel
     return await ctx.channel.send(bot_messages.command_channel_error)
 
-  if arg1==None or arg2==None or arg3==None or arg4==None: #checking parametres is entered or not entered
+  if any(params is None for params in [arg1, arg2, arg3, arg4]): #checking parametres is entered or not entered
     return await ctx.channel.send(bot_messages.parametres_error)
 
   if not arg3.isdigit():
@@ -77,12 +77,12 @@ async def add_channel(ctx,arg1: discord.TextChannel, arg2: discord.TextChannel,a
 
 
 @Bot.command()
-@has_permissions(administrator=True)
-async def show_channel(ctx, arg1=None): #show channel command
+@has_permissions(administrator = True)
+async def show_channel(ctx, arg1 = None): #show channel command
   guild_id = ctx.message.guild.id
   command_channel_ids = command_channel_list(guild_id)[0]
   channel_lists = channel_list(guild_id)
-  print(f"Şuna dak bak {channel_lists} bir de buna {command_channel_ids}")
+ 
 
   if len(command_channel_ids) == 0:
     await ctx.channel.send(bot_messages.set_command)
@@ -91,15 +91,18 @@ async def show_channel(ctx, arg1=None): #show channel command
   #if arg1 is empty, it's gonna show all channel. but if you put in something, it's gonna show that index
   if ctx.channel.id != int(command_channel_ids[0]): #checking if channel is command channel
     return await ctx.channel.send(bot_messages.command_channel_error)
+   
+  if len(channel_lists)==0: #checking list is empty or not
+    await ctx.channel.send(bot_messages.empty_channels)
+    return
+   
   if arg1 != None and int(arg1)<=len(channel_lists) and arg1.isdigit()==True: #showing channel for arg1 index
     arg1=int(arg1)
     await ctx.channel.send(
       f"**{arg1}.** **from: {Bot.get_channel(int(channel_lists[arg1-1][0])).mention}**,**to: {Bot.get_channel(int(channel_lists[arg1-1][1])).mention}**, **count: {channel_lists[arg1-1][2]}**, **emoji: {channel_lists[arg1-1][3]}**\n")
     return
   
-  if len(channel_lists)==0: #checking list is empty or not
-    await ctx.channel.send(bot_messages.empty_channels)
-    return
+  
 
   count = 1
   for i in channel_lists: #then shows channels that you added
@@ -112,31 +115,32 @@ async def show_channel(ctx, arg1=None): #show channel command
 
 
 @Bot.command()
-@has_permissions(administrator=True)
-async def delete_channel(ctx,arg1): #delete channel command
+@has_permissions(administrator = True)
+async def delete_channel(ctx, arg1): #delete channel command
 
   guild_id = ctx.message.guild.id #getting guild_id
   command_channel_ids = command_channel_list(guild_id) #getting our command channel id
   channel_lists = channel_list(guild_id) #getting our channels we set "add_channel" before
 
 
-  if len(command_channel_ids)==0:
+  if len(command_channel_ids) == 0:
     await ctx.channel.send(bot_messages.set_command)
     return
 
-  if ctx.channel.id !=int(command_channel_ids[0]):
+  if ctx.channel.id != int(command_channel_ids[0]):
     return await ctx.channel.send(bot_messages.command_channel_error)
 
-  if len(channel_lists)==0:
+  if len(channel_lists) == 0:
     await ctx.channel.send(bot_messages.empty_channels)
     return
-  if arg1.isdigit()==True:
+  if arg1.isdigit() == True:
     arg1=int(arg1)
+    
   else:
     ctx.channel.send(bot_messages.int_error)
     return
 
-  delete_channels(arg1,guild_id)
+  delete_channels(arg1, guild_id)
   await ctx.channel.send(bot_messages.channel_deleted)
 
 @Bot.event
@@ -152,28 +156,29 @@ async def on_raw_reaction_add(payload): #the work place.
 
 
   if payload.emoji.name in emoji_lists: #checking the reaction emoji is our list or not
-    index1=emoji_lists.index(payload.emoji.name) #then getting index of emoji
+    emoji_index = emoji_lists.index(payload.emoji.name) #then getting index of emoji
 
     for reaction in message.reactions:
       if reaction.emoji == payload.emoji.name: #checking the emoji is in our list or not
-        EmojiCount=reaction.count
-        if EmojiCount == int(channel_lists[index1][2]): #checking EmojiCount is equal our count or not
+        EmojiCount = reaction.count
+        if EmojiCount == int(channel_lists[emoji_index][2]): #checking EmojiCount is equal our count or not
 
-          if int(payload.channel_id)!= int(channel_lists[index1][0]): #checking channel id is equal our channel id or not
+          if int(payload.channel_id) != int(channel_lists[emoji_index][0]): #checking channel id is equal our channel id or not
             return
           if message.author.id == Bot.user.id: #checking messages's owner is bot or not
             return
-          for reaction in message.reactions:
-            if (reaction.me) and (reaction.emoji == "✅"): #checking the emoji reactioner is bot or not. this is important because bot can't do this 2rd times or much
-              return
+          if (reaction.me) and (reaction.emoji == "✅"): #checking the emoji reactioner is bot or not. this is important because bot can't do this 2rd times or much
+            return
          
 
           await Bot.get_channel(int(channel_lists[index1][1])).send( #it moves messages to selected channel
               "Written by {} :\n\n {}".format(message.author.mention, message.content),
               files=[await f.to_file() for f in message.attachments])
+          
           await Bot.get_channel(int(channel_lists[index1][0])).send( #it sends messages channel to say "that messages moved"
               "{} your message moved to this channel --> {}".format(message.author.mention,
                                                                     Bot.get_channel(int(channel_lists[index1][1])).mention))
+          
           await message.add_reaction("✅") #then adding a reaction for don't archive message again.
           return
 
